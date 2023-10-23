69277b1b7c14fe90cdd7dfa46fee6d8599baa038
dmitriy-tischenko <tischenko.prg@gmail.com>
2023-10-24 00:53:41 +0300

    gs-client gs-administrator security configuration

--- gs-administrator/src/main/java/ru/otus/example/grooming/gsadmin/configuration/security/ApiSecurityConfiguration.java
+++ gs-administrator/src/main/java/ru/otus/example/grooming/gsadmin/configuration/security/ApiSecurityConfiguration.java
@@ -1,8 +1,10 @@
 package ru.otus.example.grooming.gsadmin.configuration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,7 +12,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.otus.example.grooming.gsadmin.services.ApiUserDetailService;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableWebSecurity
@@ -48,8 +57,20 @@ public class ApiSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public SecurityFilterChain configureFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.cors();
+        httpSecurity
+                .cors(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(@Value("${security.allowed-origins}") String allowedOrigins) {
+        List<String> origins = Stream.of(allowedOrigins.split(",")).toList();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(origins);
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
