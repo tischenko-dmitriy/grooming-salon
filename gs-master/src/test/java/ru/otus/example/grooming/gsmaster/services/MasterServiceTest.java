1fc8f08504a183200d1b108dd075b9bfed9e15fe
dmitriy-tischenko <tischenko.prg@gmail.com>
2023-10-29 01:17:48 +0300

    module gs-master

---
+++ gs-master/src/test/java/ru/otus/example/grooming/gsmaster/services/MasterServiceTest.java
@@ -0,0 +1,24 @@
+package ru.otus.example.grooming.gsmaster.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MasterServiceTest {
+
+    @BeforeEach
+    public void init() {
+    }
+
+    @Test
+    public void testCreateMaster() {
+    }
+
+    @Test
+    public void testGetSchedule() {
+    }
+}
