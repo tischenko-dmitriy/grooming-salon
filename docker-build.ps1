0522a40447bb74c06396858de722dbb2c23aee84
dmitriy-tischenko <tischenko.prg@gmail.com>
2023-11-03 23:41:11 +0300

    docker-build.ps1, docker-compose

--- docker-build.ps1
+++ docker-build.ps1
@@ -4,14 +4,14 @@ mvn clean package
 
 cd gs-administrator
 docker build --tag gs-administrator .
-#docker run --rm --name gs-administrator -p 8082:8082 gs-administrator:latest
 
 cd ..\gs-client
 docker build --tag gs-client .
-#docker run --rm --name gs-client -p 8081:8081 gs-client:latest
 
 cd ..\gs-master
 docker build --tag gs-master .
-#docker run --rm --name gs-master -p 8083:8083 gs-master:latest
 
 cd ..
+
+docker-compose rm -s -f
+docker-compose up -d
