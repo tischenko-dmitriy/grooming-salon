9ba003bb5e142a39b18b8ec5188916aa44700be7
dmitriy-tischenko <tischenko.prg@gmail.com>
2023-11-09 20:58:10 +0300

    gs-client unit tests

--- gs-client/src/test/java/ru/otus/example/grooming/gsclient/controllers/GeneralControllerTest.java
+++ gs-client/src/test/java/ru/otus/example/grooming/gsclient/controllers/GeneralControllerTest.java
@@ -1,6 +1,7 @@
 package ru.otus.example.grooming.gsclient.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,9 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -18,6 +22,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import ru.otus.example.grooming.gsclient.configuration.security.ApiSecurityConfiguration;
 import ru.otus.example.grooming.gsclient.repositories.UserRepository;
 import ru.otus.example.grooming.gsclient.services.GeneralControllerService;
 
@@ -34,7 +39,9 @@ public class GeneralControllerTest {
     @Autowired
     ApplicationContext applicationContext;
 
-    //@Autowired
+    @Autowired
+    FilterChainProxy springSecurityFilterChain;
+
     private MockMvc mockMvc;
 
     @Autowired
@@ -47,15 +54,21 @@ public class GeneralControllerTest {
     public void init() {
         mockMvc = MockMvcBuilders.standaloneSetup(generalController)
                 .defaultRequest(get("/"))
+                .addFilters(springSecurityFilterChain)
+                .dispatchOptions(true)
                 .build();
-
     }
 
     @Test
-    public void contextLoads() {
+    @DisplayName("Checking application context.")
+    public void testContextLoads() {
+        assertThat(generalController).isNotNull();
+        assertThat(generalControllerService).isNotNull();
+        assertThat(applicationContext.getBean("apiSecurityConfiguration")).isInstanceOf(ApiSecurityConfiguration.class);
     }
 
     @Test
+    @DisplayName("HTTP request \"get /grooming/client/test\" (application accessibility)")
     public void doTestApplication() throws Exception {
         //given
         String uri = "/grooming/client/test";
@@ -65,7 +78,8 @@ public class GeneralControllerTest {
                 .thenReturn(appName);
 
         //when
-        MvcResult result = this.mockMvc.perform(get(uri))
+        MvcResult result = this.mockMvc.perform(get(uri)
+                        .with(SecurityMockMvcRequestPostProcessors.user("user1")))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
